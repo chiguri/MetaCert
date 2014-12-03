de8fca621c14fab71676aa90d1d38fa7e905598a
Sosuke MORIGUCHI <chiguri.s@gmail.com>
2014-12-03 10:59:34 +0900

    Add example and fix some bug in exp_meta_base.

--- extension.v
+++ extension.v
@@ -31,20 +31,16 @@ Definition syntax_exp_decl := type_decl (type_name "exp") [exp_var_constr; exp_n
 Inductive exp_meta_base : val -> exp -> Prop :=
 | exp_mb_var : forall v, exp_meta_base (con_val (con_args (con_name (constr_name "var")) (str_val v))) (var (var_name v))
 | exp_mb_num : forall z, exp_meta_base (con_val (con_args (con_name (constr_name "num")) (num_val z))) (num z)
-| exp_mb_plus : forall z1 z2,
+| exp_mb_plus : forall e1 e2 v1 v2,
+   exp_meta_base e1 v1 -> exp_meta_base e2 v2 ->
    exp_meta_base
     (con_val
      (con_args
       (con_args
        (con_name (constr_name "plus"))
-       (con_val
-        (con_args (con_name (constr_name "num"))
-                  (num_val z1))))
-      (con_val
-       (con_args
-        (con_name (constr_name "num"))
-                  (num_val z2)))))
-     (binary b_plus (num z1) (num z2))
+       e1)
+      e2))
+    (binary b_plus v1 v2)
 .
 
 
@@ -118,14 +114,103 @@ Qed.
 
 
 
-(* a := 10; a := a + 10 *)
-Definition test_program2 :=
- Passign
-  (var_name "v")
+(* v = "a := 10; a := a + 10" *)
+Definition test_program2_data :=
   (application
    (application
     (con
-     (constr_name "assign")) (str "a")) (application (con (constr_name "num")) (num 10%Z))).
+     (constr_name "seq"))
+    (application
+     (application
+      (con
+       (constr_name "assign"))
+      (str "a"))
+     (application
+      (con (constr_name "num"))
+      (num 10%Z))))
+  (application
+   (application
+    (con
+     (constr_name "assign")) (str "a"))
+   (application
+    (application
+     (con (constr_name "plus"))
+     (application
+      (con (constr_name "var"))
+      (str "a")))
+    (application
+     (con (constr_name "num"))
+     (num 10%Z))))).
+
+Definition test_program2 :=
+ Passign (var_name "v") test_program2_data.
+
+
+
+
+Goal verify [] test_program2 True (forall v p, exp_val (var (var_name "v")) v -> proc_meta_base v p -> verify [] p True (exp_val (var (var_name "a")) (num_val 20%Z))).
+unfold test_program2.
+apply Vweaken with
+ (P1 := forall (v : val) (p : proc), exp_val test_program2_data v -> proc_meta_base v p -> verify [] p True (exp_val (var (var_name "a")) (num_val 20%Z))).
+apply Vassign with (P := fun e => forall (v : val) (p : proc), exp_val e v -> proc_meta_base v p -> verify [] p True (exp_val (var (var_name "a")) (num_val 20%Z))).
+intro H; clear H.
+intros.
+unfold test_program2_data in H.
+(* evaluation of expression *)
+inversion H; clear H; subst.
+inversion H1; clear H1; subst.
+inversion H3; clear H3; subst.
+inversion H2; clear H2; subst.
+inversion H5; clear H5; subst.
+inversion H; clear H; subst.
+inversion H3; clear H3; subst.
+inversion H2; clear H2; subst.
+inversion H5; clear H5; subst.
+inversion H; clear H; subst.
+inversion H3; clear H3; subst.
+inversion H2; clear H2; subst.
+inversion H5; clear H5; subst.
+inversion H; clear H; subst.
+inversion H3; clear H3; subst.
+inversion H5; clear H5; subst.
+inversion H.
+inversion H6; clear H6; subst.
+inversion H7; clear H7; subst.
+inversion H1.
+inversion H; clear H; subst.
+inversion H3; clear H3; subst.
+inversion H2; clear H2; subst.
+inversion H5; clear H5; subst.
+inversion H; clear H; subst.
+inversion H3; clear H3; subst.
+inversion H5; clear H5; subst.
+inversion H.
+inversion H6; clear H6; subst.
+inversion H.
+inversion H8; clear H8; subst.
+inversion H; clear H; subst.
+inversion H3; clear H3; subst.
+inversion H5; clear H5; subst.
+inversion H.
+(* meta-to-base *)
+inversion H0; clear H0; subst.
+inversion H2; clear H2; subst.
+inversion H3; clear H3; subst.
+inversion H4; clear H4; subst.
+inversion H2; clear H2; subst.
+inversion H1; clear H1; subst.
+inversion H4; clear H4; subst.
+(* generated program verification *)
+apply Vweaken with (P1 := (exp_val (binary b_plus (num 10%Z) (num 10%Z)) (num_val 20%Z))).
+eapply Vseq.
+apply Vassign with (P := fun e => exp_val (binary b_plus e (num 10)) (num_val 20)).
+apply Vassign with (P := fun e => exp_val e (num_val 20)). (* なんでapplyだけじゃうまく行かないのかよく分からない *)
+intro H; clear H.
+eapply val_binop.
+apply val_num.
+apply val_num.
+constructor.
+Qed.
 
 
 
